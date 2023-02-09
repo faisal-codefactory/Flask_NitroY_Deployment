9891d246540ea4d7d99532b22816cf54ba6b4188
Sheraz Naseer <sheraz.naseer@gmail.com>
2023-02-09 15:39:26 +0500

    Added local codes to remote repository

---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, request#,flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from keras.models import load_model
+import requests
+import numpy as np
+#import extractFeatures
+import protutil
+from keras import backend as K
+import tensorflow as tf
+ 
+# App config.
+
+#cnn_model = load_model("cnnVamide2012.h5")prot
+cnn_model = load_model("NitroTyrCNNModel201226.h5")
+
+#cnn_model._make_predict_function()
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.debug = False
+#DEBUG = True
+#app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/cite')
+def cite():
+    return render_template('cite.html', title='Citation')
+
+@app.route('/supl')
+def supl():
+    return render_template('supl.html', title='Supplementary Data')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About us')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+ 
+class PredForm(Form):
+    #sequence = TextAreaField(u'Protien Sequence &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp(Enter A Fasta \
+    #    format Sequence or Just Sequence Text)', [validators.DataRequired()])
+    #sequence = TextAreaField(u'Enter Protien Sequence (Fasta or Text Format))',[validators.DataRequired()])
+    sequence = TextAreaField(u'Enter Protien Sequence (Text Format))')
+    #print (sequence)
+
+def SimpleFastaParser(fasta_sequence):
+    seq = fasta_sequence.split('\n')
+    seq = seq[1:]
+    re = ''
+    for x in seq:
+        re = re + x[:len(x)-2]
+    return re
+
+def SimpleParser(seq): #verified
+    seq ="".join(seq.splitlines())
+    return seq
+
+@app.route("/pred", methods=['GET', 'POST'])
+def pred():
+    amino_acid = "Y"
+    form = PredForm(request.form)
+    #print (form.errors)
+    if request.method == 'POST':
+        input_seq = request.form['sequence']
+        #sequence = ''
+        #if '>' in input_seq:
+        #    sequence = SimpleFastaParser(input_seq)
+        #else:
+        sequence = SimpleParser(input_seq)
+        #***********************************************************************
+        #Symbol of Amino Acid needs to be changed Here     
+        sample_list, idx_list = protutil.samplesfromProtein(amino_acid, sequence)
+        #***********************************************************************
+        #sess = tf.Session()
+        #K.set_session(sess)
+        #try:
+        #    cnn_model = load_model("cnnPhosphos.h5")
+        #except Exception as e:
+            #pass
+        #    raise e
+        #result = "Who let the dogs out"
+        #print(idx_list)
+        tmp =[protutil.encode_sample(a) for a in sample_list] 
+        X = np.array(tmp)
+        #X= np.expand_dims(X, axis=2)    
+        #X = np.array([vamideutil.encode_sample(a) for a in sample_list])
+        y = cnn_model.predict_classes(X)
+        #sess.close()
+        tmp= np.nonzero(y)[0]  # returns the index of nonzero (postive) labels in predicted y
+        pos_site_list = [idx_list[a] for a in tmp]
+        pos_seq_list = [sample_list[a] for a in tmp]
+        result = dict(zip(pos_site_list, pos_seq_list))
+        #result = extractFeatures.extractFeatures.formatSeq(sequence)
+        return resultPage(result)
+        #return resultPage(pos_seq_list, pos_site_list)
+    
+    return render_template('pred.html', form=form, title="Prediction" )
+
+def resultPage(result):
+    return render_template('sznresult.html', result=result, title="Results")
+
+
+if __name__ == "__main__":
+    #app.run("6000",debug=True)
+    app.run(host="127.0.0.1", port=32000, debug=True)
